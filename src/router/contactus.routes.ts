7010a6a13b6fa7d24f121dc1a9813815629fedb9
Pallavi sisodiya <pallavi.sisodiya9009@gmail.com>
2023-11-22 16:29:30 +0530

    campus add

--- src/router/contactus.routes.ts
+++ src/router/contactus.routes.ts
@@ -2,6 +2,6 @@ import { contactusController } from "../controller/contactus.controller";
 import { Express } from "express";
 export function ContactusRoutes(app: Express) {
   app.post("/contactus", contactusController.createContact);
-  app.get("/allcontact",contactusController.getContacts)
-  app.delete("/deletecontact/:id",contactusController.deleteContact)
+  app.get("/allcontact", contactusController.getContacts);
+  app.delete("/deletecontact/:id", contactusController.deleteContact);
 }
